3903b9e853b556ff915cfbe6cd113a495930f107
Parviz Khavari <csctclan@gmail.com>
2020-05-01 19:18:05 +0300

    Use BaseService instance in tests

--- tests/qaseio/services/test_base_service.py
+++ tests/qaseio/services/test_base_service.py
@@ -13,22 +13,22 @@ def test_empty_service_init(values):
         BaseService(*values)
 
 
-def test_vr_too_many_requests():
+def test_vr_too_many_requests(base_service):
     res = Response()
     res.status_code = 429
     res._content = b'{"status":true,"result":{}}'
     with pytest.raises(TooManyRequestsException):
-        BaseService.validate_response(res, to_type=None)
+        base_service.validate_response(res, to_type=None)
 
 
 @pytest.mark.parametrize(
     "res_status, validate_status", [(201, 201), (202, [200, 201, 202])]
 )
-def test_vr_correct_status_code(res_status, validate_status):
+def test_vr_correct_status_code(base_service, res_status, validate_status):
     res = Response()
     res.status_code = res_status
     res._content = b'{"status":true,"result":{}}'
-    data = BaseService.validate_response(
+    data = base_service.validate_response(
         res, to_type=None, status=validate_status
     )
     assert data == {}
@@ -37,42 +37,42 @@ def test_vr_correct_status_code(res_status, validate_status):
 @pytest.mark.parametrize(
     "res_status, validate_status", [(201, 20), (203, [200, 201, 202])]
 )
-def test_vr_incorrect_status_code(res_status, validate_status):
+def test_vr_incorrect_status_code(base_service, res_status, validate_status):
     res = Response()
     res.status_code = res_status
     res._content = b'{"status":true,"result":{}}'
     with pytest.raises(ValueError):
-        BaseService.validate_response(
+        base_service.validate_response(
             res, to_type=None, status=validate_status
         )
 
 
-def test_vr_res_status_false():
+def test_vr_res_status_false(base_service):
     res = Response()
     res.status_code = 200
     res._content = b'{"status":false,"result":{}}'
     with pytest.raises(ValueError):
-        BaseService.validate_response(res, to_type=None)
+        base_service.validate_response(res, to_type=None)
 
 
-def test_vr_res_not_json():
+def test_vr_res_not_json(base_service):
     res = Response()
     res.status_code = 200
     res._content = b"some none json string"
     with pytest.raises(ValueError):
-        BaseService.validate_response(res, to_type=None)
+        base_service.validate_response(res, to_type=None)
 
 
-def test_vr_res_correct_type():
+def test_vr_res_correct_type(base_service):
     res = Response()
     res.status_code = 200
     res._content = b'{"status":true,"result":{"code":"TRU"}}'
-    data = BaseService.validate_response(res, to_type=ProjectCreated)
+    data = base_service.validate_response(res, to_type=ProjectCreated)
     assert isinstance(data, ProjectCreated)
     assert data.code == "TRU"
 
 
-def test_vr_res_incorrect_type():
+def test_vr_res_incorrect_type(base_service):
     @attr.s
     class Ex:
         field = attr.ib()
@@ -81,4 +81,4 @@ def test_vr_res_incorrect_type():
     res.status_code = 200
     res._content = b'{"status":true,"result":{"codes":"TRU"}}'
     with pytest.raises(ValueError):
-        BaseService.validate_response(res, to_type=Ex)
+        base_service.validate_response(res, to_type=Ex)
