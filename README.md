a451eb274eb97f151d34b874cc4dbba5512e89af
Parviz Khavari <csctclan@gmail.com>
2021-02-12 23:38:05 +0300

    Implement initial variant
    - Support to publish test results
    - Support to publish to existing run
    - Support to create new run

--- README.md
+++ README.md
@@ -2,13 +2,70 @@
 
 [![License](https://lxgaming.github.io/badges/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)
 
-# Installation
+## Installation
 
 ```
 pip install qase-robotframework
 ```
 
-# Contribution
+## Usage
+
+You must add Qase case IDs to robot framework tests.
+They should be added as a tags in form like `Q-<case id without project code>`. Examples:
+
+```robotframework
+*** Test Cases ***
+Push button
+    [Tags]  Q-2
+    Push button    1
+    Result should be    1
+
+Push multiple buttons
+    [Tags]  Q-3
+    Push button    1
+    Push button    2
+    Result should be    12
+```
+
+```robotframework
+
+*** Test Cases ***    Expression    Expected
+Addition              12 + 2 + 2    16
+                      2 + -3        -1
+    [Tags]   Q-7
+
+Subtraction           12 - 2 - 2    8
+                      2 - -3        5
+    [Tags]   Q-8
+```
+
+After adding new tags and configuring listener - you could simply use it like this:
+
+```
+robot --listener qaseio.robotframework.Listener keyword_driven.robot data_driven.robot
+```
+
+## Configuration
+
+Listener supports loading configuration both from environment variables and from `tox.ini` file.
+
+ENV variables:
+- `QASE_API_TOKEN` - API token to access Qase TMS
+- `QASE_PROJECT` - Project code from Qase TMS
+- `QASE_RUN_ID` - Run ID if you want to add results to existing run
+- `QASE_RUN_NAME` - Set custom run name when no run ID is provided
+
+`tox.ini` configuration:
+
+```ini
+[qase]
+qase_api_token=<API TOKEN>
+qase_project=PROJECTCODE
+qase_run_id=14
+qase_run_name=New Robot Framework Run
+```
+
+## Contribution
 
 Install project locally:
 
--- src/qaseio/robotframework/__init__.py
+++ src/qaseio/robotframework/__init__.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
+import configparser
 import logging
+import os
+import re
+from datetime import datetime
+from enum import Enum
 from pkg_resources import DistributionNotFound, get_distribution
+from typing import List, TypedDict
 
+from qaseio import client
+from qaseio.client.models import (
+    TestRunCreate,
+    TestRunResultCreate,
+    TestRunResultStatus,
+    TestRunResultUpdate,
+)
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -14,3 +26,167 @@ finally:
 
 
 logger = logging.getLogger("qase-robotframework")
+
+STATUSES = {
+    "PASS": TestRunResultStatus.PASSED,
+    "FAIL": TestRunResultStatus.FAILED,
+}
+
+
+class Envs(Enum):
+    TOKEN = "QASE_API_TOKEN"
+    PROJECT = "QASE_PROJECT"
+    RUN_ID = "QASE_RUN_ID"
+    RUN_NAME = "QASE_RUN_NAME"
+
+
+class StartSuiteModel(TypedDict):
+    id: str
+    longname: str
+    doc: str
+    metadata: dict
+    source: str
+    suites: List[str]
+    tests: List[str]
+    totaltests: int
+    starttime: str
+
+
+class EndSuiteModel(TypedDict):
+    id: str
+    longname: str
+    doc: str
+    metadata: dict
+    source: str
+    starttime: str
+    endtime: str
+    elapsedtime: int
+    status: str
+    statistics: str
+    message: str
+
+
+class StartTestModel(TypedDict):
+    id: str
+    longname: str
+    originalname: str
+    doc: dict
+    tags: List[str]
+    critical: str
+    template: str
+    lineno: int
+    starttime: str
+
+
+class EndTestModel(TypedDict):
+    id: str
+    longname: str
+    originalname: str
+    doc: dict
+    tags: List[str]
+    critical: str
+    template: str
+    lineno: int
+    starttime: str
+    endtime: str
+    elapsedtime: int
+    status: str
+    message: str
+
+
+class StartKeywordModel(TypedDict):
+    type: str
+    kwname: str
+    libname: str
+    doc: dict
+    args: List[str]
+    assign: List[str]
+    tags: List[str]
+    starttime: str
+
+
+class EndKeywordModel(TypedDict):
+    type: str
+    kwname: str
+    libname: str
+    doc: dict
+    args: List[str]
+    assign: List[str]
+    tags: List[str]
+    starttime: str
+    endtime: str
+    elapsedtime: int
+    status: str
+
+
+class Listener:
+    ROBOT_LISTENER_API_VERSION = 2
+
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("tox.ini")
+
+        self.token = self.get_param(Envs.TOKEN)
+        self.project_code = self.get_param(Envs.PROJECT)
+        if not self.token or not self.project_code:
+            raise ValueError("Token and Project code should be provided")
+        self.api = client.QaseApi(self.token)
+        self.run_id = self.get_param(Envs.RUN_ID)
+        self.run_name = self.get_param(Envs.RUN_NAME)
+        self.results = {}
+
+    def get_param(self, param: Envs):
+        param: str = param.value
+        return os.environ.get(
+            param, self.config.get("qase", param.lower(), fallback=None)
+        )
+
+    def start_suite(self, name, attributes: StartSuiteModel):
+        if not self.run_id and attributes.get("totaltests") > 0:
+            name = self.run_name or "Automated Run {}".format(
+                str(datetime.now())
+            )
+            logger.info("Creating new run with name: %s", name)
+            res = self.api.runs.create(
+                self.project_code, TestRunCreate(name, [])
+            )
+            self.run_id = res.id
+        elif self.run_id:
+            logger.info("Using run %s to publish test results", self.run_id)
+
+    def start_test(self, name, attributes: StartTestModel):
+        ids = self._extract_ids(attributes.get("tags"))
+        data = {"hashes": []}
+        for _id in ids:
+            logger.info("Running test %s-%s", self.project_code, _id)
+            req_data = TestRunResultCreate(
+                _id, TestRunResultStatus.IN_PROGRESS
+            )
+            res = self.api.results.create(
+                self.project_code, self.run_id, req_data
+            )
+            data.get("hashes").append(res.hash)
+        self.results[attributes.get("id")] = data
+
+    def end_test(self, name, attributes: EndTestModel):
+        for hash in self.results.get(attributes.get("id"), {}).get(
+            "hashes", []
+        ):
+            req_data = TestRunResultUpdate(
+                STATUSES[attributes.get("status")],
+                time=attributes.get("elapsedtime"),
+                stacktrace=attributes.get("message"),
+            )
+            self.api.results.update(
+                self.project_code, self.run_id, hash, req_data
+            )
+
+    def log_message(self, message):
+        logger.debug("Log:", message)
+
+    def _extract_ids(self, list_of_tags: List[str]) -> List[int]:
+        return [
+            int(re.match(r"Q-(\d+)", tag).groups()[0])
+            for tag in list_of_tags
+            if re.fullmatch(r"Q-\d+", tag)
+        ]
