5ff16aa6161a97f272c097a60e6be2679e262953
Kamil Wartanowicz <kamil@plume.com>
2023-12-19 13:09:41 +0100

    Fix reruns to consider only the last report

--- qaseio/qaseio/utils/result.py
+++ qaseio/qaseio/utils/result.py
@@ -12,18 +12,23 @@ class Result(Run):
         super().__init__(project=project, parent_suite_id=parent_suite_id, token=token)
         self.results_api = ResultsApi(self.client)
 
-    def get_results(self, status=None, **data):
-        """Get test results for selected test run"""
+    def get_results(self, status=None, last_result=True, **data):
+        """
+        Get test results for selected test run
+        By default only the last result published in test run is considered.
+        """
 
         all_results = []
         if "limit" not in data:
             data["limit"] = API_LIMIT
-        if status:
+        if status and not last_result:
             data["status"] = status
+
+        # Get number_of_results with limit=1 for faster response
         one_data = data.copy()
         one_data["limit"] = 1
         number_of_results = \
-        get_result(self.results_api.get_results(code=self.project, run=str(self.run_id), **one_data))["filtered"]
+            get_result(self.results_api.get_results(code=self.project, run=str(self.run_id), **one_data))["filtered"]
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
             data_iter = [(data, offset) for offset in range(0, number_of_results, API_LIMIT)]
@@ -33,6 +38,15 @@ class Result(Run):
                     all_results.extend(entities)
                 else:
                     break
+        if last_result:
+            cases_results = {}
+            for result in all_results:
+                case_id = result["case_id"]
+                if case_id not in cases_results or cases_results[case_id]["end_time"] < result["end_time"]:
+                    cases_results[case_id] = result
+            all_results = [result for _, result in cases_results.items()]
+            if status:
+                all_results = [result for result in all_results if result["status"].lower() == status.lower()]
         return all_results
 
     def _get_results(self, data_iter):
