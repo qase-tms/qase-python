531a4d9ec1bcc2f0024f77583ea19a2379892756
Kamil Wartanowicz <kamil@plume.com>
2024-02-02 17:15:09 +0100

    Improve exception handling for invalid qase WEB_API cookies

--- qaseio/qaseio/utils/web/web.py
+++ qaseio/qaseio/utils/web/web.py
@@ -1,6 +1,13 @@
+from json import JSONDecodeError
+
 from qaseio.utils.web.web_api import WebApi
 
 
 class Web(WebApi):
     def get_run_info(self, project, run_id):
-        return self.get(f"project/{project}/run/{run_id}/dashboard/info")
+        try:
+            return self.get(f"project/{project}/run/{run_id}/dashboard/info")
+        except JSONDecodeError:
+            raise EnvironmentError(
+                "Update envs for cookie values: QASE_WEB_HOST_SESSION, QASE_WEB_SESSION and QASE_WEB_SRF_TOKEN"
+            )
