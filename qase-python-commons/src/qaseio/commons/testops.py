8e52ae8f27f3a8b70459cc940980ba1f27e24218
Nick Volynkin <nick.volynkin@gmail.com>
2024-03-11 15:57:37 +0200

    Fix 'object is not subscriptable' in commons
    Resolves the following error
     TypeError: 'Environment' object is not subscriptable
    
    This error was caused by an update in qaseio client, changing the
    interface of the Environment object. Now qase-python-commons supports
    the new interface.

--- qase-python-commons/src/qaseio/commons/testops.py
+++ qase-python-commons/src/qaseio/commons/testops.py
@@ -91,8 +91,8 @@ class QaseTestOps:
             response = api_instance.get_environments(code=project)
             if hasattr(response, 'result'):
                 for env in response.result.entities:
-                    if env["slug"] == environment:
-                        return env["id"]
+                    if env.slug == environment:
+                        return env.id
         return None
 
     def _send_bulk_results(self):
