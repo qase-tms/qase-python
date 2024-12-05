127d9f770e8a030e3eeca426ab6968c51fb5ed82
Dmitrii Gridnev <dgridnev@qase.io>
2024-12-05 16:26:34 +0100

    test: add unit test

---
+++ qase-tavern/tests/test_extractor.py
@@ -0,0 +1,50 @@
+from qase.tavern.plugin import QasePytestPlugin
+
+
+def test_extract_qase_id_with_valid_id():
+    text = "QaseID=123 Some text"
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id == 123
+    assert remaining_text == "Some text"
+
+
+def test_extract_qase_id_with_valid_id_lowercase():
+    text = "qaseid=456 More text"
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id == 456
+    assert remaining_text == "More text"
+
+
+def test_extract_qase_id_with_no_qaseid():
+    text = "No QaseID here"
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id is None
+    assert remaining_text == text
+
+
+def test_extract_qase_id_with_spaces():
+    text = "   QaseID= 321   Text with spaces   "
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id == 321
+    assert remaining_text == "Text with spaces"
+
+
+def test_extract_qase_id_with_only_qaseid():
+    text = "QaseID=999"
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id == 999
+    assert remaining_text == ""
+
+
+def test_extract_qase_id_with_special_characters():
+    text = "Some text QaseID=123!@#$"
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id == 123
+    assert remaining_text == "Some text !@#$"
+
+
+def test_extract_qase_id_with_empty_string():
+    text = ""
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id is None
+    assert remaining_text == ""
