f21968754e93c8bb8a69be732747c65c60e7edff
Dmitrii Gridnev <dgridnev@qase.io>
2025-03-10 15:23:50 +0100

    feat: add support for multiple IDs linking a single test to multiple test cases
    - Implemented the ability to specify multiple test case IDs for a single automated test.
    - Enhanced test case linking for better traceability and organization.

--- qase-tavern/tests/test_extractor.py
+++ qase-tavern/tests/test_extractor.py
@@ -4,47 +4,52 @@ from qase.tavern.plugin import QasePytestPlugin
 def test_extract_qase_id_with_valid_id():
     text = "QaseID=123 Some text"
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id == 123
+    assert qase_id == [123]
     assert remaining_text == "Some text"
 
+def test_extract_multiple_qase_id_with_valid_id():
+    text = "QaseID=123,321 Some text"
+    qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
+    assert qase_id == [123,321]
+    assert remaining_text == "Some text"
 
 def test_extract_qase_id_with_valid_id_lowercase():
     text = "qaseid=456 More text"
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id == 456
+    assert qase_id == [456]
     assert remaining_text == "More text"
 
 
 def test_extract_qase_id_with_no_qaseid():
     text = "No QaseID here"
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id is None
+    assert qase_id == []
     assert remaining_text == text
 
 
 def test_extract_qase_id_with_spaces():
     text = "   QaseID= 321   Text with spaces   "
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id == 321
+    assert qase_id == [321]
     assert remaining_text == "Text with spaces"
 
 
 def test_extract_qase_id_with_only_qaseid():
     text = "QaseID=999"
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id == 999
+    assert qase_id == [999]
     assert remaining_text == ""
 
 
 def test_extract_qase_id_with_special_characters():
     text = "Some text QaseID=123!@#$"
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id == 123
+    assert qase_id == [123]
     assert remaining_text == "Some text !@#$"
 
 
 def test_extract_qase_id_with_empty_string():
     text = ""
     qase_id, remaining_text = QasePytestPlugin.extract_qase_id(text)
-    assert qase_id is None
+    assert qase_id == []
     assert remaining_text == ""
