6609502fd8228025134b120c36f5e3867b1b841f
Yan <yanfedorov120505@gmail.com>
2025-11-09 18:59:17 +0100

    feat(qase-pytest: commons): fix str env var comparing to int

--- qase-python-commons/src/qase/commons/models/config/batch.py
+++ qase-python-commons/src/qase/commons/models/config/batch.py
@@ -1,13 +1,23 @@
+from typing import Union
+
 from ..basemodel import BaseModel
 
 
 class BatchConfig(BaseModel):
-    size: int = None
+    size: int
 
     def __init__(self):
         self.size = 200
 
-    def set_size(self, size: int):
-        if size > 2000 or size == 0:
+    def set_size(self, size: Union[int, str]):
+        try:
+            if isinstance(size, str):
+                size = size.strip()
+                size_int = int(size)
+            else:
+                size_int = size
+            if size_int > 2000 or size_int == 0:
+                self.size = size_int
+        except ValueError:
             raise ValueError("Batch size should be greater than 0 and less than 2000")
-        self.size = size
+
